e7049cc1a7b5898efa24fdf4141f885eeb1f556a
xhowl1 <christopher@analyticsfire.com>
2020-01-03 16:06:31 +0800

    Added bee and clouds

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -7,7 +7,7 @@ int main()
 	VideoMode vm(1920, 1080);
 
 	// Create and open a window for the game
-	RenderWindow window(vm, "Timber!!!", Style::Resize);
+	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
 
 	// Create a texture to hold a graphinc on the GPU
 	Texture textureBackground;
@@ -42,18 +42,164 @@ int main()
 	// How fast can the bee fly
 	float beeSpeed = 0.0f;
 
+	// Make 3 cloud sprites from 1 texture
+	Texture textureCloud;
+	textureCloud.loadFromFile("graphics/cloud.png");
+	// 3 New sprites with the same texture
+	Sprite spriteCloud1;
+	Sprite spriteCloud2;
+	Sprite spriteCloud3;
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+	// Position the clouds off screen
+	spriteCloud1.setPosition(0, 0);
+	spriteCloud2.setPosition(0, 250);
+	spriteCloud3.setPosition(0, 500);
+	// Are the clouds currently on screen?
+	bool cloud1Active = false;
+	bool cloud2Active = false;
+	bool cloud3Active = false;
+	// How fast is each cloud?
+	float cloud1Speed = 0.0f;
+	float cloud2Speed = 0.0f;
+	float cloud3Speed = 0.0f;
+
+	Clock clock;
+
 	while (window.isOpen())
 	{
-		if (Keyboard::isKeyPressed(Keyboard::Escape)) 
+		if (Keyboard::isKeyPressed(Keyboard::Escape) || Mouse::isButtonPressed(Mouse::Button::Left)) 
 		{
 			window.close();
 		}
+
+		// Measure time
+		Time dt = clock.restart();
+
+		// Setup the bee
+		if (!beeActive)
+		{
+			// How fast is the bee
+			srand((int)time(0) * 10);
+			beeSpeed = (rand() % 200) + 200;
+
+			// How high is the bee
+			srand((int)time(0) * 10);
+			float height = (rand() % 500) + 500;
+			spriteBee.setPosition(2000, height);
+			beeActive = true;
+		} 
+		else
+		{
+			spriteBee.setPosition(
+				spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()),
+				spriteBee.getPosition().y
+			);
+
+			// Has the bee reached the right hand edge of the screen?
+			if (spriteBee.getPosition().x < -100)
+			{
+				beeActive = false;
+			}
+		}
+
+		// Manage the clouds
+		// Cloud 1
+		if (!cloud1Active)
+		{
+			// How fast is the cloud
+			srand((int)time(0) * 10);
+			cloud1Speed = (rand() % 200);
+
+			// How high is the cloud
+			srand((int)time(0) * 10);
+			float height = (rand() % 150);
+			spriteCloud1.setPosition(-200, height);
+			cloud1Active = true;
+		}
+		else
+		{
+			spriteCloud1.setPosition(
+				spriteCloud1.getPosition().x + (cloud1Speed * dt.asSeconds()),
+				spriteCloud1.getPosition().y
+			);
+
+			if (spriteCloud1.getPosition().x > 1920)
+			{
+				cloud1Active = false;
+			}
+		}
+
+		// Cloud 2
+		if (!cloud2Active)
+		{
+			// How fast is the cloud
+			srand((int)time(0) * 20);
+			cloud2Speed = (rand() % 200);
+
+			// How high is the cloud
+			srand((int)time(0) * 20);
+			float height = (rand() % 300) - 150;
+			spriteCloud2.setPosition(-200, height);
+			cloud2Active = true;
+		}
+		else
+		{
+			spriteCloud2.setPosition(
+				spriteCloud2.getPosition().x + (cloud2Speed * dt.asSeconds()),
+				spriteCloud2.getPosition().y
+			);
+
+			if (spriteCloud2.getPosition().x > 1920)
+			{
+				cloud2Active = false;
+			}
+		}
+
+		// Cloud 3
+		if (!cloud3Active)
+		{
+			// How fast is the cloud
+			srand((int)time(0) * 30);
+			cloud3Speed = (rand() % 200);
+
+			// How high is the cloud
+			srand((int)time(0) * 30);
+			float height = (rand() % 450) - 150;
+			spriteCloud3.setPosition(-200, height);
+			cloud3Active = true;
+		}
+		else
+		{
+			spriteCloud3.setPosition(
+				spriteCloud3.getPosition().x + (cloud3Speed * dt.asSeconds()),
+				spriteCloud3.getPosition().y
+			);
+
+			if (spriteCloud3.getPosition().x > 1920)
+			{
+				cloud3Active = false;
+			}
+		}
+
 
 		// Clear everything for the last frame
 		window.clear();
 
 		// Draw our game scene here
 		window.draw(spriteBackground);
+
+		// Draw the clouds
+		window.draw(spriteCloud1);
+		window.draw(spriteCloud2);
+		window.draw(spriteCloud3);
+
+		// Draw the tree
+		window.draw(spriteTree);
+
+		// Draw the insect
+		window.draw(spriteBee);
 
 		// Show everything we just drew
 		window.display();
